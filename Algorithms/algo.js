40b4d74ba4372839a5d94cb1f937083b4b9b0e9b
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-06-09 16:12:27 -0400

    update

--- Algorithms/algo.js
+++ Algorithms/algo.js
@@ -1180,3 +1180,25 @@ function maxSubArraySumSlow(arr, num) {
   }
   return max;
 }
+//We first add the first three values in the array (if num = 3)
+//We compare by moving the window, subtracting the first element and adding the next element.
+//In the for loop the initialization of i only occurs once
+//
+function maxSubArraySumFast(arr, num) {
+  let maxSum = 0;
+  let tempSum = 0;
+  if (num > arr.length) {
+    return null;
+  }
+  for (let i = 0; i < num; i++) {
+    maxSum += arr[i];
+  }
+  tempSum = maxSum;
+  for (let i = num; i < arr.length; i++) {
+    tempSum = tempSum - arr[i - num] + arr[i];
+    maxSum = Math.max(maxSum, tempSum);
+  }
+  return maxSum;
+}
+
+console.log(maxSubArraySumFast([2, 6, 9, 2, 1, 8, 5, 6, 3], 3));
